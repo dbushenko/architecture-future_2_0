bfb3150aaa08df26d64632a3bb5ea09cbfe8ee9d
dbushenko <d.bushenko@gmail.com>
2026-02-14 07:55:16 +0300

    experiments ended

--- Task1Advanced/main.tf
+++
@@ -1,5 +0,0 @@
-data "kubectl_server_version" "current" {}
-
-output "server_version" {
-  value = data.kubectl_server_version.current.version
-}
--- Task1Advanced/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-minikube_driver = "docker"
-minikube_memory = 4096
-minikube_cpus   = 2
